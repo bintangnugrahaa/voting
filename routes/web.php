f5b880877d34584049d14fabd798699bfab3264f
bintangnugraha <djakarta1973@gmail.com>
2025-02-23 10:38:19 +0700

    feat: Add voting route to dashboard
    - Added `/vote/{candidate}` route in `DashboardController`
    - Enables users to vote for a candidate

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ Route::get('/', function () {
 
 Route::group(['prefix' => 'app', 'as' => 'app.', 'middleware' => 'auth'], function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/vote/{candidate}', [DashboardController::class, 'vote'])->name('vote');
 
     Route::resource('candidate', CandidateController::class);
     Route::resource('voter', VoterController::class);
