deea16ace75553b7960559a98da8332a00bb5634
bintangnugraha <djakarta1973@gmail.com>
2025-02-23 10:41:10 +0700

    feat: add voting functionality and candidate sorting
    - Implement vote method to allow users to cast votes
    - Include vote count in candidate data
    - Sort candidates by predefined order

--- app/Http/Controllers/Web/DashboardController.php
+++ app/Http/Controllers/Web/DashboardController.php
@@ -4,12 +4,22 @@ namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
 use App\Models\Candidate;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        $candidates = Candidate::all();
+        $candidates = Candidate::withCount('votes')->get()->sortBy('sort_order');
         return view('pages.app.dashboard', compact('candidates'));
+    }
+
+    public function vote(Candidate $candidate)
+    {
+        $candidate->votes()->create([
+            'voter_id' => Auth::user()->voter->id,
+        ]);
+
+        return redirect()->route('app.dashboard')->with('success', 'Voting Successfully Conducted');
     }
 }
